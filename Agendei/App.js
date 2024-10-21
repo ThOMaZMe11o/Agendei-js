cf1c25d105a418603d450fcc106d8c6b2ec49100
ThOMaZMe11o <thomazmello1912@gmail.com>
2024-10-21 18:15:41 -0300

    create pages, add @react-navigation/native and bottom-tabs

--- Agendei/App.js
+++ Agendei/App.js
@@ -1,5 +1,7 @@
 import Login from "./src/screens/login/login.jsx";
 import Account from "./src/screens/account/account.jsx";
+import Home from "./src/screens/Home/home.jsx"
+import Calendar from "./src/screens/Calendar/calendar.jsx";
 
 function App() {
 
--- Agendei/src/components/Button/button.jsx
+++ Agendei/src/components/Button/button.jsx
@@ -3,11 +3,13 @@ import { styles } from "./button.style.js";
 
 function Button(props) {
 
-    function TesteClick() {
-        Alert.alert("Clicou no nosso botão");
-    }
+    return <TouchableOpacity
 
-    return <TouchableOpacity onPress={TesteClick} style={styles.btn}>
+        style={[styles.btn,
+        props.theme == "danger" ?
+            styles.danger : styles.primary]}
+
+        onPress={props.onPress} >
         <Text style={styles.text}>
             {props.text}
         </Text>
